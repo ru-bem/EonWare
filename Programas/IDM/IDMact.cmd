e6b080033251db5b47e0d044c2972fd9a2d258dc
Rubem Júnior <60861729+rubem-psd@users.noreply.github.com>
2024-01-03 08:57:39 -0300

    EonWare

--- Programas/IDM/IDMact.cmd
+++ Programas/IDM/IDMact.cmd
@@ -192,19 +192,6 @@ setlocal EnableDelayedExpansion
 
 ::========================================================================================================================================
 
-echo "!_batf!" | find /i "!_ttemp!" %nul1% && (
-if /i not "!_work!"=="!_ttemp!" (
-%eline%
-echo Script is launched from the temp folder,
-echo Most likely you are running the script directly from the archive file.
-echo:
-echo Extract the archive file and launch the script from the extracted folder.
-goto done2
-)
-)
-
-::========================================================================================================================================
-
 ::  Elevate script as admin and pass arguments and preventing loop
 
 %nul1% fltmc || (
