3569372bb005f9f5bca5f0bcf0839360fe298134
Rubem Júnior <60861729+rubem-psd@users.noreply.github.com>
2024-02-04 09:20:32 -0300

    Update IDMact.cmd

--- Programas/IDM/IDMact.cmd
+++ Programas/IDM/IDMact.cmd
@@ -644,7 +644,7 @@ exit /b
 
 set /a attempt=0
 if exist "%file%" del /f /q "%file%"
-start "" /B "%IDMan%" /n /d "%link%" /p "%SystemRoot%\Temp" /f temp.png
+start "" /MIN /B "%IDMan%" /n /d "%link%" /p "%SystemRoot%\Temp" /f temp.png
 
 :check_file
 
